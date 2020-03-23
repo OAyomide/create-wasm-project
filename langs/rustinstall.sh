8d954db34fb8d1190f66f7095702d985f8472c5b
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2020-03-23 16:11:39 +0100

    rustinstall.sh: bash script to install rust stuff

--- langs/rustinstall.sh
+++ langs/rustinstall.sh
@@ -1,4 +1,58 @@
 #!/bin/bash
 
-rust -V foo >/dev/null 2>&1 ||
-{ echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+function install_curl {
+    curlv=`curl -V`;
+    if [ -z "$curlv" ];
+    then
+        echo "cURL not installed...Installing cURL first"
+        sudo apt update && sudo apt upgrade
+        sudo apt install curl -y
+        echo "##########################"
+        echo "cURL has been installed..."
+    else
+        echo "cURL has already been installed"
+    fi
+}
+
+function install_wasmpack {
+    install_curl
+    wasmpackv=`wasm-pack -V`;
+    
+    if [ -z "$wasmpackv" ];
+    then
+        echo "Rust has been installed but wasm-pack has not. Proceeding to install wasmpack"
+        curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+        echo " "
+        echo "################################################"
+        echo "wasm-pack has been installed.."
+    else
+        echo ""
+        echo "################################################"
+        echo "wasm-pack has been installed.."
+    fi
+}
+
+function install_rust {
+    rustv=`rustc -V`;
+    if [ -z "$rustv" ];
+    then
+        # here is where we install rust
+        echo "Rust has not been installed... "
+        install_curl
+        echo "cURL already installed, proceeding to install rustc"
+        echo "################################################"
+        echo " "
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        echo " "
+        echo "################################################"
+        echo "Successfully installed Rust"
+    else
+        echo "Rust has been installed...skipping"
+    fi
+    
+    install_wasmpack
+    echo ""
+    echo "Seems you're ready to go. Everything needed has been installed"
+}
+
+install_rust
--- langs/rustlang.go
+++ langs/rustlang.go
@@ -1,1 +1,23 @@
 package langs
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path"
+)
+
+// we want to run the rustinstall bash script
+
+func Install_deps() {
+	// file, err := filepath.Abs(filepath.Dir("./rustinstall.sh"))\
+	file := path.Dir("rustinstall.sh")
+	fmt.Printf("%s\n", file)
+	out, err := exec.Command("/bin/bash", "../langs/rustinstall.sh").CombinedOutput()
+
+	if err != nil {
+		log.Fatalf("Error running the bash script...: %s", err.Error())
+	}
+
+	fmt.Printf("output of running rustinstall.sh: %s", string(out))
+}
